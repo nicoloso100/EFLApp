3bd694d11da4f959cdfc73f360b34721910d9d47
David Ruiz <davidl.ruizc@gmail.com>
2020-02-15 10:10:10 -0500

    getting the results and adding the clean async storage for score

--- src/views/activity1/step4.js
+++ src/views/activity1/step4.js
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { StyleSheet, View, Alert, ScrollView, RefreshControl, Text } from 'react-native';
 import {Icon, Button} from 'react-native-elements';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import {primaryColor, bodyColor1} from '../colors';
+import { bodyColor1 } from '../colors';
 import HomeButton from '../../components/HomeButton';
 import ActivityResults from '../../components/ActivityResults';
 
@@ -25,31 +26,100 @@ const excercise = [
 ];
 
 const Step4 = ({ navigation }) => {
-  const onButtonClick = () => {
-    Alert.alert('Warning', 'Are you sure that want to continue?')
+  const [refreshing, setRefreshing] = useState(false);
+  const [allResults, setAllResults] = useState([]);
+
+  /**
+   * Timeout promise
+   * 
+   * @param {number} timeout 
+   */
+  const wait = (timeout) => {
+    return new Promise(resolve => {
+      setTimeout(resolve, timeout);
+    });
+  }
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+
+    wait(200).then(async () => {
+      const firstActivityCorrect = await AsyncStorage.getItem('FirstActivityCorrect');
+      const firstActivityIncorrect = await AsyncStorage.getItem('FirstActivityIncorrect');
+      const secondActivityCorrect = await AsyncStorage.getItem('SecondActivityCorrect');
+      const secondActivityIncorrect = await AsyncStorage.getItem('SecondActivityIncorrect');
+
+      setAllResults([
+        {
+          id: 1,
+          correct: firstActivityCorrect ? firstActivityCorrect : '0',
+          incorrect: firstActivityIncorrect ? firstActivityIncorrect : '0'
+        },
+        {
+          id: 2,
+          correct: secondActivityCorrect ? secondActivityCorrect : '0',
+          incorrect: secondActivityIncorrect ? secondActivityIncorrect : '0'
+        }
+      ]);
+      setRefreshing(false);
+    });
+  }, [refreshing]);
+
+  const onButtonClick = async () => {
+    try {
+      Alert.alert(
+        'Attendez!',
+        'Êtes-vous sûr de vouloir supprimer votre score?',
+        [
+          {
+            text: 'Annuler',
+            style: 'cancel',
+          },
+          {text: `D'accord`, onPress: async () => {
+            await AsyncStorage.removeItem('FirstActivityCorrect');
+            await AsyncStorage.removeItem('FirstActivityIncorrect');
+            await AsyncStorage.removeItem('SecondActivityCorrect');
+            await AsyncStorage.removeItem('SecondActivityIncorrect');
+          }},
+        ],
+        {cancelable: false},
+      );
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   return(
-    <View style={styles.container}>
-      <HomeButton navigate={navigation.navigate} />
-      <ActivityResults
-        excercise={excercise}
-      />
-      <Button
-        buttonStyle={styles.button}
-        titleStyle={styles.buttonIcon}
-        icon={
-          <Icon
-            name="refresh"
-            type="font-awesome"
-            size={30}
-            color="white"
-          />
-        }
-        title="Refresh"
-        onPress={() => onButtonClick()}
-      />
-    </View>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+        />
+      }
+    >
+      <View style={styles.containers}>
+        <HomeButton navigate={navigation.navigate} />
+        <ActivityResults
+          excercise={allResults}
+        />
+        <Button
+          buttonStyle={styles.button}
+          titleStyle={styles.buttonIcon}
+          icon={
+            <Icon
+              name="refresh"
+              type="font-awesome"
+              size={30}
+              color="white"
+            />
+          }
+          title="Réinitialiser le score"
+          onPress={() => onButtonClick()}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
