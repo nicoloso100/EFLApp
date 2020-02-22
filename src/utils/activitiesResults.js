fc4eab6c6dfabdd72d01f3193b8d500b0bfc3ab7
David Ruiz <davidl.ruizc@gmail.com>
2020-02-22 10:44:32 -0500

    adding functions for other activities and listing in the object

--- src/utils/activitiesResults.js
+++ src/utils/activitiesResults.js
@@ -23,3 +23,68 @@ export const getResultsFirstActivity = async () => {
   }
   return result;
 }
+
+export const getResultsSecondActivity = async () => {
+  const firstActivityCorrect = await AsyncStorage.getItem('SecondActivityFirstStepCorrect');
+  const firstActivityIncorrect = await AsyncStorage.getItem('SecondActivityFirstStepIncorrect');
+  const secondActivityCorrect = await AsyncStorage.getItem('SecondActivitySecondStepCorrect');
+  const secondActivityIncorrect = await AsyncStorage.getItem('SecondActivitySecondStepIncorrect');
+  const thirdActivityCorrect = await AsyncStorage.getItem('SecondActivityThirdStepCorrect');
+  const thirdActivityIncorrect = await AsyncStorage.getItem('SecondActivityThirdStepIncorrect');
+
+  let allCorrectResults = parseInt(
+      firstActivityCorrect !== null ? firstActivityCorrect : 0
+    ) + 
+    parseInt(
+      secondActivityCorrect !== null ? secondActivityCorrect : 0
+    ) + 
+    parseInt(
+      thirdActivityCorrect !== null ? thirdActivityCorrect : 0
+    );
+
+  let allIncorrectResults = parseInt(
+      firstActivityIncorrect !== null ? firstActivityIncorrect : 0
+    ) + 
+    parseInt(
+      secondActivityIncorrect !== null ? secondActivityIncorrect : 0
+    ) + 
+    parseInt(
+      thirdActivityIncorrect !== null ? thirdActivityIncorrect : 0
+    );
+
+  let result = {
+    detailed: {
+      firstActivityCorrect: firstActivityCorrect !== null ? parseInt(firstActivityCorrect) : 0,
+      firstActivityIncorrect: firstActivityIncorrect !== null ? parseInt(firstActivityIncorrect) : 0,
+      secondActivityCorrect: secondActivityCorrect !== null ? parseInt(secondActivityCorrect) : 0,
+      secondActivityIncorrect: secondActivityIncorrect !== null ? parseInt(secondActivityIncorrect) : 0,
+      thirdActivityCorrect: thirdActivityCorrect !== null ? parseInt(thirdActivityCorrect) : 0,
+      thirdActivityIncorrect: thirdActivityIncorrect !== null ? parseInt(thirdActivityIncorrect) : 0,
+    },
+    summarized:{
+      allCorrectResults, 
+      allIncorrectResults
+    }
+  }
+  return result;
+}
+
+export const getResultsThirdActivity = async () => {
+  const firstActivityCorrect = await AsyncStorage.getItem('ThirdActivityFirstStepCorrect');
+  const firstActivityIncorrect = await AsyncStorage.getItem('ThirdActivityFirstStepIncorrect');
+
+  let allCorrectResults = parseInt(firstActivityCorrect !== null ? firstActivityCorrect : 0);
+  let allIncorrectResults = parseInt(firstActivityIncorrect !== null ? firstActivityIncorrect : 0);
+  
+  let result = {
+    detailed: {
+      firstActivityCorrect: firstActivityCorrect !== null ? parseInt(firstActivityCorrect) : 0,
+      firstActivityIncorrect: firstActivityIncorrect !== null ? parseInt(firstActivityIncorrect) : 0,
+    },
+    summarized:{
+      allCorrectResults, 
+      allIncorrectResults
+    }
+  }
+  return result;
+}
