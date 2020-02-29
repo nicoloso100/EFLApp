19898a93953b91de625502f0dd07081a7f285077
David Ruiz <davidl.ruizc@gmail.com>
2020-02-29 13:56:20 -0500

    changing application ID

--- android/app/build/generated/source/buildConfig/release/com/eflapp/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/eflapp/BuildConfig.java
@@ -5,7 +5,7 @@ package com.eflapp;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.eflapp";
+  public static final String APPLICATION_ID = "com.eflappulibre";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
