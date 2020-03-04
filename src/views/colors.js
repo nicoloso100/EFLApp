63b8b91f67e502234369405e086c65717effac2d
Nicolas Angarita Ortiz <Nicolas.Angarita@sinco.com.co>
2020-03-04 13:40:19 -0500

    cleaning project

--- src/views/colors.js
+++
@@ -1,5 +0,0 @@
-export const primaryColor = '#113d72';
-export const navBarColor = '#789DD0';
-export const bodyColor1 = '#B5C9E5';
-export const bodyColor2 = '#A1BDE5';
-export const navIconColor = '#BFCEE5';
